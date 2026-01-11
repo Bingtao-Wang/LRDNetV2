991eaa93c02a7f9a375d50d3482b6185f1e8b165
Bingtao-Wang <2358036494@qq.com>
2026-01-11 21:50:39 +0800

    TF-GPU run: Using Size 1280 x 384 The training is not good.

--- train_gpu.py
+++ train_gpu.py
@@ -79,7 +79,6 @@ def dice_coef(y_true, y_pred):
 def dice_coef_loss(y_true, y_pred):
     return 1.0 - dice_coef(y_true, y_pred)
 
-
 def iou_coef(y_true, y_pred):
     smooth = 1e-5
     # 强制转换为 float32 进行计算
